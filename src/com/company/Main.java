1a259f6514e84365000c3dc60f821ebe771e5643
Kamrul Islam Tushar <kamrul.islam@reddotdigitalit.com>
2022-11-19 21:41:49 +0600

    even odd code added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world!");
+        int n = 100;
+        if (n %2 == 0) {
+            System.out.println("Even number");
+        }
     }
 }
